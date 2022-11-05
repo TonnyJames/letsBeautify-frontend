b18910fbe56c8067fd0c4b3316d02aec9fdffc7c
Tonny James P. dos Reis <tonny.jreis@outlook.com>
2022-11-05 20:35:14 -0300

    criação e implementação de consultaComponent

---
+++ src/app/components/api-relatorio/consulta/consulta.component.ts
@@ -0,0 +1,38 @@
+import { ConsultaService } from './../../../services/consulta.service';
+import { Router } from '@angular/router';
+import { FormControl, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-consulta',
+  templateUrl: './consulta.component.html',
+  styleUrls: ['./consulta.component.css']
+})
+export class ConsultaComponent implements OnInit {
+
+
+
+  cpf: string = '';
+
+  cpfControl = new FormControl(null, Validators.required)
+
+  constructor(
+    private router: Router,
+    private service: ConsultaService
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  consultar() {
+    // this.service.findByCpf(this.cpf).subscribe(resposta => {})
+    this.router.navigate(['/api/'+ this.cpf])
+    console.log("consultar clicado")
+  }
+  
+
+  validaCampos(): boolean {
+    return this.cpfControl.valid
+  }
+
+}
