de3ad4e909f7640464c224e63c08cda96b277146
Tonny James P. dos Reis <tonny.jreis@outlook.com>
2022-11-16 22:51:03 -0300

    novo componente servico-list

---
+++ src/app/components/servico/servico-list/servico-list.component.ts
@@ -0,0 +1,43 @@
+import { Servico } from './../../../models/servico';
+import { ServicoService } from './../../../services/servico.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Cliente } from 'src/app/models/cliente';
+import { ClienteService } from 'src/app/services/cliente.service';
+
+@Component({
+  selector: 'app-servico-list',
+  templateUrl: './servico-list.component.html',
+  styleUrls: ['./servico-list.component.css']
+})
+export class ServicoListComponent implements OnInit {
+  ELEMENT_DATA: Servico[] = []
+
+  displayedColumns: string[] = ['id', 'categoria', 'nmNegocio', 'telefone', 'email', 'nomeAdmin', 'acoes'];
+  dataSource = new MatTableDataSource<Servico>(this.ELEMENT_DATA);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(
+    private service: ServicoService
+  ) { }
+
+  ngOnInit(): void {
+    this.findAll();
+  }
+
+  findAll(){
+    this.service.findAll().subscribe(resposta => {
+      this.ELEMENT_DATA = resposta
+      this.dataSource = new MatTableDataSource<Servico>(resposta);
+      this.dataSource.paginator = this.paginator;
+    })
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+}
